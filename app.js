89946c6ad98e18c86ecb6ba6cdbecec92370d3ce
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-05-17 16:54:31 -0400

    4:54 p.m.

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
 
 const app = angular.module('NewChatApp', []);
 
-app.controller('ChatAppController', function($scope, ChatService) {
+app.controller('ReceiveMessageController', function ($scope, ChatService) {
 
+    $scope.newMessages = ChatService.getChats();
 
+    $scope.messageNow = ChatService.getNewChats();
 
-});
-
-app.controller('NewMessageController', function($scope, ChatService){
-
-
-});
-
-app.factory('ChatService', function($http){
-    let messages = [];
-
-    let mostRecent = {
-        sender: 'Username',
-        messageContent: 'Message text.'
+    $scope.readChat = function(chat) {
+        ChatService.readChat(chat);
     };
 
-    $http.get('https://tiy-28202.herokuapp.com/chats').then(function(response){
+});
 
-        
+app.controller('SendMessageController', function ($scope, ChatService) {
 
+
+
+});
+
+app.factory('ChatService', function ($http) {
+    let newMessages = [];
+
+    let messageNow = {
+        from: 'Username',
+        message: 'Message text.',
+        id: 'number',
+        added: 'time',
+    };
+
+    $http.get('https://tiy-28202.herokuapp.com/chats').then(function (response) {
+
+        const statements = response.data.chats; 
+        for (let i = 0; i < statements.length; i++) {
+            newMessages.push({
+                from: statements[i].from,
+                message: statements[i].message,
+                id: statements[i].id,
+                added: statements[i].added,
+            });
+        }; 
+        console.log(newMessages);
     });
 
 
-
-
-})
+    return {
+        getChats(){
+            return newMessages;
+        },
+        getNewChats(){
+            return messageNow;
+        },
+        readChat(chat){
+            messageNow.message = chat.message;
+        },
+    };
+});
